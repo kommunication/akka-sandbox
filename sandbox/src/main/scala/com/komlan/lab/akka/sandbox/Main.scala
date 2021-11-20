4e1b54109e579078505b1181148ab6c537e824d9
kommunication <komlan@gmail.com>
2021-11-19 23:43:37 -0500

    sandbox project

---
+++ sandbox/src/main/scala/com/komlan/lab/akka/sandbox/Main.scala
@@ -0,0 +1,10 @@
+package com.komlan.lab.akka
+package sandbox
+
+object Main extends App {
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
+}
---
+++ sandbox/src/test/scala/com/komlan/lab/akka/sandbox/ExampleSuite.scala
@@ -0,0 +1,8 @@
+package com.komlan.lab.akka
+package sandbox
+
+final class ExampleSuite extends TestSuite {
+  test("hello world") {
+    1 shouldBe 1
+  }
+}
